ad217eddc04dacc5017ea55b96db0a2565cb1409
olexandryermilov@gmail.com <oleksandry@wix.com>
2022-11-14 23:24:20 +0100

    add translation to mask replacement

--- bert-mlm-replacement.py
+++ bert-mlm-replacement.py
@@ -9,8 +9,9 @@ import spacy
 from transformers import pipeline
 
 NER = spacy.load("en_core_web_sm")
-#DE_NER = spacy.load("de_core_news_sm")
-mask_predictor = pipeline('fill-mask')
+DE_NER = spacy.load("de_core_news_sm")
+en_mask_predictor = pipeline('fill-mask')
+german_mask_predictor = pipeline('fill-mask', model = "uklfr/gottbert-base")
 
 entity_map = dict()
 placeholders_map = dict()
@@ -29,7 +30,7 @@ def choose_word(original, predictions):
             return word['token_str']
     return original.text
 
-def bert_mask_replacement(corpus, f, ner):
+def bert_mask_replacement(corpus, f, ner, mask_predictor):
   processed = ner(corpus)
   res = []
   result = 0
@@ -92,37 +93,50 @@ def write(f, text):
 
 def writeFileJSONAnon(filePath, content, anonFunc, task):
     f = open(filePath, "a")
-    #f.write('{"data":[')
+    if task == "translation":
+        f.write('{"data":[')
     f.close()
     result = 0
     if task == "summarization":
         ner_source = NER
         ner_target = NER
+        mask_source = en_mask_predictor
+        mask_target = en_mask_predictor
+        first_col_name = "text"
+        second_col_name = "summary"
     elif task == "translation":
         ner_source = NER
         ner_target = DE_NER
+        mask_source = en_mask_predictor
+        mask_target = german_mask_predictor
+        first_col_name = "en"
+        second_col_name = "de"
     else:
         ner_source = NER
         ner_target = NER
-
+        mask_source = en_mask_predictor
+        mask_target = en_mask_predictor
+        first_col_name = "text"
+        second_col_name = "summary"
     for e in tqdm(content[:-1]):
         f = open(filePath, "a")
-        f.write('{"text": "')
-        result += anonFunc(e[0].replace('"', '').replace('\\','').replace('\t',''), f, ner_source)
-        f.write('","summary": "')
-        result += anonFunc(e[1].replace('"', '').replace('\\','').replace('\t',''), f, ner_target)
+        f.write('{"'+first_col_name+'": "')
+        result += anonFunc(e[0].replace('"', '').replace('\\','').replace('\t',''), f, ner_source, mask_source)
+        f.write('","'+second_col_name+'": "')
+        result += anonFunc(e[1].replace('"', '').replace('\\','').replace('\t',''), f, ner_target, mask_target)
         f.write('"},\n')
         f.close()
     f = open(filePath, "a")
     for e in content[-1:]:
         f = open(filePath, "a")
-        f.write('{"text": "')
-        anonFunc(e[0].replace('"', '').replace('\\', '').replace('\t', ''), f, ner_source)
-        f.write('","summary": "')
-        anonFunc(e[1].replace('"', '').replace('\\', '').replace('\t', ''), f, ner_target)
+        f.write('{"' + first_col_name + '": "')
+        anonFunc(e[0].replace('"', '').replace('\\', '').replace('\t', ''), f, ner_source, mask_source)
+        f.write('","'+second_col_name+'": "')
+        anonFunc(e[1].replace('"', '').replace('\\', '').replace('\t', ''), f, ner_target, mask_target)
         f.write('"}\n')
 
-    #f.write(']}')
+    if task == "translation":
+        f.write(']}')
     f.close()
     with open("../all_ners.txt", "w") as f1:
         f1.write(str(result/len(content)))
@@ -146,11 +160,11 @@ def processFile(filePath, anonymize, methodFunc, methodName, task, name):
         source = readFile(filePath+'.source').split("\n")
         target = readFile(filePath+'.target').split("\n")
     else:
-        source = readFile(filePath).split("\n")
-        target = readFile(filePath).split("\n")
+        source = readFile(filePath+'.en').split("\n")
+        target = readFile(filePath+'.de').split("\n")
     together = list(zip(source, target))
     if anonymize:
-        writeFileJSONAnon(name+"_anonymized_spacy_"+methodName+".json", together, methodFunc, task)
+        writeFileJSONAnon(name+"_anonymized_"+task+"_spacy_"+methodName+".json", together, methodFunc, task)
     else:
         writeFileJSON(filePath.split(".")[0]+".json", together)
     return
@@ -161,13 +175,15 @@ def main():
     print(sys.argv)
     task = sys.argv[1]
     train = sys.argv[2]
-    val = sys.argv[3]
-    test = sys.argv[4]
+    if task == "summarization":
+        val = sys.argv[3]
+    test = sys.argv[-1]
     methodName = "ner-mask"
     method = bert_mask_replacement
     processFile(train, True, method, methodName, task, 'train')
-    processFile(test, True, method, methodName, task,'test')
-    processFile(val, True, method, methodName, task,'val')
+    processFile(test, True, method, methodName, task, 'test')
+    if task == "summarization":
+        processFile(val, True, method, methodName, task, 'val')
     with open(f'placeholders_spacy_{task}.json', 'w') as f:
         f.write(json.dumps(entity_map))
 main()
