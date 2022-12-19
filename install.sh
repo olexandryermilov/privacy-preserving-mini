c3c1439dd1cea73e210dba3905cb251c2dbbfeeb
oleksandryermilov <olexandr.yermilov@gmail.com>
2022-12-19 09:35:58 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ pip3 install datasets
 pip3 install evaluate
 pip3 install rouge_score
 pip3 install git+https://github.com/huggingface/transformers
+pip3 install spacy
+pip3 install torch
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download de_core_news_sm
