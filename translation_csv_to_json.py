52740d0c44d2ab3bf3b5974c95ae5008256b44d9
oleksandryermilov <olexandr.yermilov@gmail.com>
2023-01-15 14:18:43 +0100

    update scripts

--- translation_csv_to_json.py
+++ translation_csv_to_json.py
@@ -6,8 +6,8 @@ def process_file(path):
     file = open(path, 'r')
     new_file = open(path+".json", "w")
     lines = file.readlines()
-    new_file.write('{"data":[\n')
-    for line in tqdm(lines[:-1]):
+    #new_file.write('{"data":[\n')
+    for line in tqdm(lines):
         if(line[0] =='"'):
             obj = line.split('","')
             if len(obj)<2:
@@ -18,22 +18,22 @@ def process_file(path):
             obj = line.split(",")
         print(line)
         try:
-            new_file.write('{"translation": {"en":"' + obj[0].replace('"', '') +'", "de":"'+ obj[1][:-1].replace('"', '')+'"}},\n')
+            new_file.write('{"translation": {"en":"' + obj[0].replace('"', '') +'", "de":"'+ obj[1][:-1].replace('"', '')+'"}}\n')
         except:
             print(line)
             continue
-    for line in lines[-1:]:
-        if (line[0] == '"'):
-            obj = line.split('","')
-            if len(obj) < 2:
-                obj = line.split('",')
-                if (len(obj) < 2):
-                    obj = line.split(',"')
-        else:
-            obj = line.split(",")
+    #for line in lines[-1:]:
+    #    if (line[0] == '"'):
+    #        obj = line.split('","')
+    #        if len(obj) < 2:
+    #            obj = line.split('",')
+    #            if (len(obj) < 2):
+    #                obj = line.split(',"')
+    #    else:
+    #        obj = line.split(",")
 
-        new_file.write('{"translation": {"en":"' + obj[0].replace('"', '') + '", "de":"' + obj[1][:-1].replace('"', '') + '"}}\n')
-    new_file.write(']}')
+    #   new_file.write('{"translation": {"en":"' + obj[0].replace('"', '') + '", "de":"' + obj[1][:-1].replace('"', '') + '"}}\n')
+    #new_file.write(']}')
 
 
 process_file(sys.argv[1])
