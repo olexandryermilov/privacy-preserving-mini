96a6021114a7f6b02fd5b11780f20a666c4209c4
oleksandryermilov <olexandr.yermilov@gmail.com>
2023-01-15 22:18:35 +0100

    add check json script

--- translation_csv_to_json.py
+++ translation_csv_to_json.py
@@ -16,7 +16,7 @@ def process_file(path):
                 if(len(obj) < 2):
                     obj = line.split(",")
         try:
-            new_file.write('{"translation": {"en":"' + obj[0].replace('"', '').replace("'","").replace("\\", "") +'", "de":"'+ obj[1][:-1].replace('"', '').replace("\\", "").replace("'","")+'"}}\n')
+            new_file.write('{"translation": {"en":"' + obj[0].replace('"', '').replace("'","").replace("\\", "").replace("12. 	Vorbereitung", "12. Vorbereitung").replace("13. 	Vorbereitung", "13. Vorbereitung") +'", "de":"'+ obj[1][:-1].replace('"', '').replace("\\", "").replace("'","").replace("12. 	Vorbereitung", "12. Vorbereitung").replace("13. 	Vorbereitung", "13. Vorbereitung")+'"}}\n')
         except:
             print(line)
             continue
