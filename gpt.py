857cbef1d974dd09fdb03b52706558408bff8b20
oleksandryermilov <olexandr.yermilov@gmail.com>
2023-03-04 17:16:14 +0100

    add script for gpt processing

---
+++ gpt.py
@@ -0,0 +1,42 @@
+import csv
+import logging
+import openai
+import sys
+
+openai_key = ""
+
+def get_response(text):
+    openai.api_key = openai_key
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Replace every named entity in the sentence using named entities of the same type. Do not change other words"},
+            #{"role": "user", "content": "Replace: Sasha lives in Germany."},
+            #{"role": "assistant", "content": "Answer: Yaroslav lives in Poland."},
+            #{"role": "user", "content": "Replace: I will meet Artem tomorrow at the Google office in New York."},
+            #{"role": "assistant", "content": "Answer: I will meet Kyrylo tomorrow at the Facebook office in San Francisco."},
+            #{"role": "user", "content": "Replace: I asked Lena from Twitter to reply to my email."},
+            #{"role": "assistant", "content": "Answer: I asked Yana from Tiktok to reply to my email."},
+            {"role": "user", "content": "Replace:" + text},
+        ]
+    )
+    return response
+
+def process_text(text) -> str:
+    resp = get_response(text)
+    response = resp['choices'][0]['message']['content'].strip()
+
+    return response
+
+# Insert the text you want to get the outputs here
+#text = "I will go to Paris, France with Daniel. I met Daniel in Starbucks and talked to him about Harry Potter films and Harry Potter franchise main star, Emma Watson."
+#process_text(text)
+file_path = sys.argv[1]
+with open(file_path, "r") as input_file, open(file_path.split(".")[0]+"_gpt35.txt", "w") as output_file:
+    # Loop through each line in the input file
+    for line in input_file:
+        # Apply the process_line function to the current line
+        processed_line = process_text(line)
+        # Write the processed line to the output file
+        output_file.write(processed_line)
+        output_file.write("\n")
--- install.sh
+++ install.sh
@@ -11,3 +11,7 @@ pip3 install sklearn
 pip3 install scikit-learn
 python3 -m spacy download en_core_web_sm
 python3 -m spacy download de_core_news_sm
+pip3 install flatbuffers
+pip3 install fuzzywuzzy
+pip3 install aiodogstatsd
+pip3 install sacrebleu
